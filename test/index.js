5071c1f1fd50807f505f59a61d6d3e08157166f6
Takeshi Iwana <iwatakeshi@gmail.com>
2015-04-09 07:33:38 -0600

    Update
    Translated files from ES5 to ES6.
    Added babel to grunt.
    Added basic tests
    Added travis

---
+++ test/index.js
@@ -0,0 +1,17 @@
+/* global describe, it*/
+var assert = require('chai').assert;
+var Core = require('gengojs-core');
+var api = require('../');
+describe('API (plugin)', function() {
+  'use strict';
+  describe('load plugin', function() {
+    it('should exist in the core', function() {
+      var gengo = new Core({}, api());
+      assert.isDefined(gengo.plugins.apis[0]);
+    });
+  });
+
+  describe('API', function() {
+
+  });
+});
